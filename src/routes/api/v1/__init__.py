ac360156cbad4ea9ecf60f46bf57ba3f2fd03dc3
ElJoamy <josephsalguero1@upb.edu>
2025-04-30 18:02:09 -0400

    chore: add __init__.py files to enable module imports
    Add __init__.py files in src, src/services, src/utils, and src/routes/api/v1 directories to make them importable as Python packages. This ensures proper module resolution and path handling for imports within the project.

---
+++ src/routes/api/v1/__init__.py
@@ -0,0 +1,28 @@
+from . import login
+from . import register
+from . import profile
+from . import logout
+from . import token_refresh
+from . import movies
+from . import genres
+
+from fastapi import APIRouter
+import sys
+import os
+
+# Permite importar desde carpetas superiores
+sys.path.append(os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "../../")))
+
+router = APIRouter()
+
+# Rutas Auth
+router.include_router(register.router, tags=["Auth"])
+router.include_router(login.router, tags=["Auth"])
+router.include_router(logout.router, tags=["Auth"])
+router.include_router(token_refresh.router, tags=["Auth"])
+
+# Rutas de perfil de usuario
+router.include_router(profile.router, tags=["User"])
+router.include_router(movies.router)
+router.include_router(genres.router)
